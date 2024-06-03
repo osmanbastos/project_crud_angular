02758f4cd67d9100ba74c35a613bf4f483ecc98b
osmanbastos <osmanbastos@hotmail.com>
2024-06-03 16:44:32 -0300

    feat: created product/(create, delete, read, update)

---
+++ src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-read',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-read.component.html',
+  styleUrl: './product-read.component.css'
+})
+export class ProductReadComponent {
+
+}
